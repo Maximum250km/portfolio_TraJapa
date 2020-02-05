17bab42d8e05d536895493e7af2c66b93ebe91d1
so mitake <player724@gmail.com>
2020-02-05 18:51:24 +0900

    POST/index now 70%

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,5 @@
 class PostsController < ApplicationController
 
-  def top
-  end
-
-  def about
-  end
 
   def new
     @post_new = Post.new
@@ -12,7 +7,7 @@ class PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.all
+    @popular_posts = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(5).pluck(:post_id))
     @user = current_user
   end
 
