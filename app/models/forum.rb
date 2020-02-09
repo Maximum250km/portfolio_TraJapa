2a6c63f075b3d662990b5488a534479114d09821
so mitake <player724@gmail.com>
2020-02-09 16:47:49 +0900

    top and footer built

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -6,6 +6,9 @@ class Forum < ApplicationRecord
 	enum forum_genre: {
 		Travel: 0,Tips: 1,Spot: 2,Trouble: 3
 	}
+  validates :forum_title, presence: true,length: {maximum:50}
+  validates :forum_body, presence: true, length: {maximum: 200}
+	validates :forum_body, presence: {message: "error"}
 
   def favorited_by?(user)
     self.forum_favorites.where(user_id: user.id).exists?
