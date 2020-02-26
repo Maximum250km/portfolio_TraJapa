98a096a23a3a7903b7fb059a745c385147cef881
so mitake <player724@gmail.com>
2020-02-26 11:32:31 +0900

    comment about JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -52,20 +52,15 @@ $(document).ready(function () {
   });
 });
 
-// tizu
 function initMap() {
-//地図を表示する領域の div 要素のオブジェクトを変数に代入
   var target = document.getElementById('gmap');
- //HTMLに記載されている住所の取得
   var address = document.getElementById("spot").textContent;
-  //ジオコーディングのインスタンスの生成
   var geocoder = new google.maps.Geocoder();
-  //geocoder.geocode() にアドレスを渡して、コールバック関数を記述して処理
+  //spotはpost/newおよびpost/editで入れた住所情報
     geocoder.geocode({ address: address }, function(results, status){
         //ステータスが OK で results[0] が存在すれば、地図を生成
       if (status === 'OK' && results[0]){
         var map = new google.maps.Map(target, {
-        //results[0].geometry.location に緯度・経度のオブジェクトが入っている
          center: results[0].geometry.location,
          zoom: 17
          });
@@ -76,7 +71,7 @@ function initMap() {
           });
         }else{
            //ステータスが OK 以外の場合や results[0] が存在しなければ、アラートを表示して処理を中断
-           //下の記述でエラーの出し方を定義している。 今回は非表示にするためコメントアウト
+           //下の記述でエラーの出し方を定義している。 今回は非表示にするためコメントアウトにて対応
            //alert('失敗しました。理由: ' + status);
            //return;
          }
